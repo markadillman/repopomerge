9baf11c0bd2691124ff78225206f0dea4a22526e
toni-york <toni.york@gmail.com>
2017-05-29 15:39:21 -0400

    Toni trying to fix crafty panning take 7

--- public/src/game.js
+++ public/src/game.js
@@ -348,7 +348,7 @@ Game =
 						if (this.x > currentUpperLeftX + tileWidth)
 						{
 							currentUpperLeftX = currentUpperLeftX + tileWidth;
-							Crafty.viewport.pan(tileWidth + canvasEdge*2, 0, panTime);
+							Crafty.viewport.pan(tileWidth + canvasEdge, 0, panTime);
 
 							// Load assets in outer rightmost "ring" segment
 							dynamicPostRequest('/pullright',payload,dynamicPostOnLoad,dynamicError);
@@ -357,7 +357,7 @@ Game =
 						else if (this.x < currentUpperLeftX)
 						{
 							currentUpperLeftX = currentUpperLeftX - tileWidth;
-							Crafty.viewport.pan((tileWidth + canvasEdge*2)*-1, 0, panTime);
+							Crafty.viewport.pan((tileWidth + canvasEdge)*-1, 0, panTime);
 
 							// Load assets in outer leftmost "ring" segment
 							dynamicPostRequest('/pullleft',payload,dynamicPostOnLoad,dynamicError);
@@ -367,7 +367,7 @@ Game =
 						if (this.y > currentUpperLeftY + tileHeight)
 						{
 							currentUpperLeftY = currentUpperLeftY + tileHeight;
-							Crafty.viewport.pan(0, tileHeight + canvasEdge*2, panTime);
+							Crafty.viewport.pan(0, tileHeight + canvasEdge, panTime);
 
 							// Load assets in outer bottom-most "ring" segment
 							dynamicPostRequest('/pullbottom',payload,dynamicPostOnLoad,dynamicError);
@@ -376,7 +376,7 @@ Game =
 						else if (this.y < currentUpperLeftY)
 						{
 							currentUpperLeftY = currentUpperLeftY - tileHeight;
-							Crafty.viewport.pan(0, (tileHeight + canvasEdge*2)*-1, panTime);
+							Crafty.viewport.pan(0, (tileHeight + canvasEdge)*-1, panTime);
 
 							// Load assets in outer top-most "ring" segment
 							dynamicPostRequest('/pulltop',payload,dynamicPostOnLoad,dynamicError);
