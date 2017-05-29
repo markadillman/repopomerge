c546900b9e3008c55368303b6e5198c726a4238e
toni-york <toni.york@gmail.com>
2017-05-29 15:32:12 -0400

    Toni trying to fix crafty panning take 2

--- public/src/game.js
+++ public/src/game.js
@@ -357,7 +357,7 @@ Game =
 						else if (this.x < currentUpperLeftX)
 						{
 							currentUpperLeftX = currentUpperLeftX - tileWidth;
-							Crafty.viewport.pan((tileWidth + canvasEdge) * -1, 0, panTime);
+							Crafty.viewport.pan(tileWidth*-1 + canvasEdge, 0, panTime);
 
 							// Load assets in outer leftmost "ring" segment
 							dynamicPostRequest('/pullleft',payload,dynamicPostOnLoad,dynamicError);
@@ -376,7 +376,7 @@ Game =
 						else if (this.y < currentUpperLeftY)
 						{
 							currentUpperLeftY = currentUpperLeftY - tileHeight;
-							Crafty.viewport.pan(0, (tileHeight + canvasEdge) * -1, panTime);
+							Crafty.viewport.pan(0, tileHeight*-1 + canvasEdge, panTime);
 
 							// Load assets in outer top-most "ring" segment
 							dynamicPostRequest('/pulltop',payload,dynamicPostOnLoad,dynamicError);
