3fe22b0a8c49ebe5f296722186b33771ec1537dd
toni-york <toni.york@gmail.com>
2017-06-03 13:59:21 -0400

    Toni trying something to load bground first take 12

--- public/src/game.js
+++ public/src/game.js
@@ -413,7 +413,7 @@ function loadPlayer() {
 		yCoord = playerSpawnY;
 		firstWorldEntry = false;
 		// use a delay to make *sure* the background is loaded first
-		Craft.e('Delay').delay(function(){}, 100, 0);
+		Crafty.e('Delay').delay(function(){}, 100, 0);
 	} else {
 		xCoord = currentPlayerX;
 		yCoord = currentPlayerY;
